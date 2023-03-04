4280e989089e2c608e4833cbb91a72d7776a354b
arjuniyer01 <iyer9@wisc.edu>
2023-03-04 17:15:33 -0600

    base version

---
+++ pages/Dashboard.py
@@ -0,0 +1,85 @@
+import streamlit as st
+import time
+from streamlit_echarts import st_echarts
+import auth
+import webbrowser
+
+if 'user_email' not in st.session_state:
+    st.session_state.user_email = None
+
+if not st.session_state.user_email:
+    if st.button("Login"):
+        webbrowser.open_new_tab(auth.get_login_str())
+elif st.session_state.user_email:
+    # st.balloons()
+    st.markdown(f"`{st.session_state.user_email}`")
+
+try:
+    st.session_state.user_email = auth.display_user()
+except Exception as e:
+    pass
+
+st.markdown('# :house: Dashboard')
+
+# TODO: Get array from DB
+content = {1677964891: 'kefgvwksjdfbwlvrsihfahemfbsldfh', 
+           1677964892: 'aekugfoquelgafvbcqoukeafgaejf,ghwkegfj', 
+           1677964893: 'keyajfgbquakefhakefavciqykjeafgqkeagf'}
+
+# TODO: Get user's role from DB
+role = st.selectbox('Role', ['student', 'teacher'])
+
+if role == 'student':
+    # TODO: Get progress from DB
+    st.markdown('## Progress')
+    for key, value in content.items():
+        with st.expander(f"{time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(key))}: {value[0:10]}..."):
+            st.write(value)
+elif role == 'teacher':
+    # TODO: Get teacher's dashboarding info from DB
+    option1 = {
+        "xAxis": {
+            "type": "category",
+            "data": ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+        },
+        "yAxis": {"type": "value"},
+        "series": [{"data": [820, 932, 901, 934, 1290, 1330, 1320], "type": "line"}],
+    }
+    st_echarts(
+        options=option1, height="400px",
+    )
+    option2 = {
+        "legend": {"top": "bottom"},
+        "toolbox": {
+            "show": True,
+            "feature": {
+                "mark": {"show": True},
+                "dataView": {"show": True, "readOnly": False},
+                "restore": {"show": True},
+                "saveAsImage": {"show": True},
+            },
+        },
+        "series": [
+            {
+                "name": "面积模式",
+                "type": "pie",
+                "radius": [50, 250],
+                "center": ["50%", "50%"],
+                "roseType": "area",
+                "itemStyle": {"borderRadius": 8},
+                "data": [
+                    {"value": 40, "name": "rose 1"},
+                    {"value": 38, "name": "rose 2"},
+                    {"value": 32, "name": "rose 3"},
+                    {"value": 30, "name": "rose 4"},
+                    {"value": 28, "name": "rose 5"},
+                    {"value": 26, "name": "rose 6"},
+                    {"value": 22, "name": "rose 7"},
+                    {"value": 18, "name": "rose 8"},
+                ],
+            }
+        ],
+    }
+    st_echarts(
+        options=option2, height="600px",
+    )
