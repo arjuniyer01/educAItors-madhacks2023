87d17ccbd64f096ddc5485bb624dae7d831c5c73
arjuniyer01 <iyer9@wisc.edu>
2023-03-05 03:09:33 -0600

    avatar

--- avatar.py
+++ avatar.py
@@ -1,49 +1,27 @@
+import os
+from google.cloud import texttospeech
 import streamlit as st
-import requests
-from PIL import Image
-from io import BytesIO
-import json
 
-# Set Streamlit page title and layout
-st.set_page_config(page_title="Roblox Avatar Animation Player")
+# set up credentials for Google Cloud TTS
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'imposing-elixir-379617-9f22e9edde76.json'
 
-# Set Streamlit app title and header
-st.title("Roblox Avatar Animation Player")
-st.header("Enter an animation ID to see it in action")
+def generate_voice(text: str):
+    # create a client object for Google Cloud TTS
+    client = texttospeech.TextToSpeechClient()
+    # set up default voice configuration
+    voice = texttospeech.VoiceSelectionParams(
+        language_code='hi-IN', ssml_gender=texttospeech.SsmlVoiceGender.FEMALE
+    )
+    # set up default audio configuration
+    audio_config = texttospeech.AudioConfig(
+        audio_encoding=texttospeech.AudioEncoding.MP3
+    )
+    # set up the synthesis input object
+    synthesis_input = texttospeech.SynthesisInput(text=text)
+    # generate the audio file
+    response = client.synthesize_speech(
+        input=synthesis_input, voice=voice, audio_config=audio_config
+    )
+    # stream the audio file to the user's browser
+    st.audio(response.audio_content, format='audio/mp3')
 
-# Get user input for animation ID
-animation_id = st.text_input("Enter animation ID")
-
-# Handle user input
-if animation_id:
-    # Construct the URL for the Roblox API request
-    url = f"https://api.roblox.com/marketplace/productinfo?assetId={animation_id}"
-
-    # Send the request to the Roblox API and parse the JSON response
-    response = requests.get(url)
-    data = json.loads(response.text)
-
-    # Check if the animation is valid
-    if "Name" in data:
-        # Get the animation name and thumbnail image URL from the response
-        animation_name = data["Name"]
-        thumbnail_url = f"https://www.roblox.com/asset-thumbnail/image?assetId={animation_id}&width=420&height=420"
-
-        # Get the thumbnail image from the URL and display it
-        response = requests.get(thumbnail_url)
-        thumbnail = Image.open(BytesIO(response.content))
-        st.subheader(animation_name)
-        st.image(thumbnail)
-
-        # Play the animation when the user presses the "Play" button
-        if st.button("Play"):
-            st.info("Playing animation...")
-            playback_url = f"https://assetgame.roblox.com/Asset/Animate.ashx?ID={animation_id}"
-            st.video(playback_url)
-    else:
-        # Display an error message if the animation is not found
-        st.error("Animation not found. Please enter a valid animation ID.")
-
-
-
-#streamlit run avatar.py
