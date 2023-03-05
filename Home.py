fc1e91a44e7de60e2850304b428b5c5fe38cce47
arjuniyer01 <iyer9@wisc.edu>
2023-03-05 09:48:26 -0600

    ai test

--- Home.py
+++ Home.py
@@ -59,56 +59,58 @@ with col2:
 user_input = st.text_input("User Input", label_visibility='hidden', placeholder="Paste text here to summarize")
 uploaded_file = st.file_uploader("File Upload", label_visibility='hidden', type=["png", "jpeg", "jpg", "mp3", "pdf", "docx"])
 
-if user_input:
-    with st_lottie_spinner(lottie_object, key="download", width=250):
-        st.write(f"{st.session_state.settings['language']}")
-        ui.process_result(f"Summarize the below text in {st.session_state.settings['language']}, explain like I am {st.session_state.settings['age']} years old in one paragraph. {user_input}")
-        ui.display_result()
-        ui.save_result()
-elif uploaded_file:
-    if uploaded_file.name.endswith(".mp3"):
-        with st.spinner("Summarizing audio input..."):
-            audio = pydub.AudioSegment.from_mp3(uploaded_file)
-            audio.export("audio.mp3", format='mp3')
-            audio_bytes = open("audio.mp3", 'rb').read()
-            st.audio(audio_bytes, format=f'audio/.mp3', start_time=0)
-            whisper_text = ai.get_text_from_whisper()["text"]
-            ui.process_result(f"Summarize the below text in {st.session_state.settings['language']}, explain like I am {st.session_state.settings['age']} years old in one paragraph. {whisper_text}")
+try:
+    if user_input:
+        with st_lottie_spinner(lottie_object, key="download", width=150):
+            ui.process_result(f"Summarize the below text in {st.session_state.settings['language']}, explain like I am {st.session_state.settings['age']} years old in one paragraph. {user_input}")
             ui.display_result()
             ui.save_result()
+    elif uploaded_file:
+        if uploaded_file.name.endswith(".mp3"):
+            with st_lottie_spinner(lottie_object, key="download", width=150):
+                audio = pydub.AudioSegment.from_mp3(uploaded_file)
+                audio.export("audio.mp3", format='mp3')
+                audio_bytes = open("audio.mp3", 'rb').read()
+                st.audio(audio_bytes, format=f'audio/.mp3', start_time=0)
+                whisper_text = ai.get_text_from_whisper()["text"]
+                ui.process_result(f"Summarize the below text in {st.session_state.settings['language']}, explain like I am {st.session_state.settings['age']} years old in one paragraph. {whisper_text}")
+                ui.display_result()
+                ui.save_result()
 
-    if uploaded_file.name.endswith(".pdf"):
-        with st.spinner("Summarizing PDF input..."):
-            pdf_text = file_reader.read_pdf(uploaded_file)
-            ui.process_result(f"Summarize the below text in {st.session_state.settings['language']}, explain like I am {st.session_state.settings['age']} years old in one paragraph. {pdf_text}")
-            ui.display_result()
-            ui.save_result()
+        if uploaded_file.name.endswith(".pdf"):
+            with st_lottie_spinner(lottie_object, key="download", width=150):
+                pdf_text = file_reader.read_pdf(uploaded_file)
+                ui.process_result(f"Summarize the below text in {st.session_state.settings['language']}, explain like I am {st.session_state.settings['age']} years old in one paragraph. {pdf_text}")
+                ui.display_result()
+                ui.save_result()
 
-    if uploaded_file.name.endswith(".docx"):
-        with st.spinner("Summarizing DOCX input..."):
-            docx_text = file_reader.convert_docx_to_text(uploaded_file)
-            ui.process_result(f"Summarize the below text in {st.session_state.settings['language']}, explain like I am {st.session_state.settings['age']} years old in one paragraph. {docx_text}")
-            ui.display_result()
-            ui.save_result()
+        if uploaded_file.name.endswith(".docx"):
+            with st_lottie_spinner(lottie_object, key="download", width=150):
+                docx_text = file_reader.convert_docx_to_text(uploaded_file)
+                ui.process_result(f"Summarize the below text in {st.session_state.settings['language']}, explain like I am {st.session_state.settings['age']} years old in one paragraph. {docx_text}")
+                ui.display_result()
+                ui.save_result()
 
-    if uploaded_file.name.endswith(".png") or uploaded_file.name.endswith(".jpeg") or uploaded_file.name.endswith(".jpg"):
-        selected = st.selectbox("Select image processing method", ["Object detection", "OCR"])
-        if st.button("Process"):
-            if selected == "Object detection":
-                with st.spinner("Summarizing using object detection..."):
-                    img = Image.open(uploaded_file)
-                    labels, img = image_processing.detect_objects(img.copy())
-                    st.image(img)
-                    ui.process_result(f"Define the objects mentioned below in {st.session_state.settings['language']}, explain like I am {st.session_state.settings['age']} years old in one paragraph. {' '.join(labels)}")
-                    ui.display_result()
-                    ui.save_result()
-            if selected == "OCR":
-                with st.spinner("Summarizing using OCR..."):
-                    img = Image.open(uploaded_file)
-                    ocr_text = image_processing.run_ocr(img.copy())
-                    st.write(ocr_text)
-                    ui.process_result(f"Summarize the below text in {st.session_state.settings['language']}, explain like I am {st.session_state.settings['age']} years old in one paragraph. {ocr_text}")
-                    ui.display_result()
-                    ui.save_result()
+        if uploaded_file.name.endswith(".png") or uploaded_file.name.endswith(".jpeg") or uploaded_file.name.endswith(".jpg"):
+            selected = st.selectbox("Select image processing method", ["Object detection", "OCR"])
+            if st.button("Process"):
+                if selected == "Object detection":
+                    with st_lottie_spinner(lottie_object, key="download", width=150):
+                        img = Image.open(uploaded_file)
+                        labels, img = image_processing.detect_objects(img.copy())
+                        st.image(img)
+                        ui.process_result(f"Define the objects mentioned below in {st.session_state.settings['language']}, explain like I am {st.session_state.settings['age']} years old in one paragraph. {' '.join(labels)}")
+                        ui.display_result()
+                        ui.save_result()
+                if selected == "OCR":
+                    with st_lottie_spinner(lottie_object, key="download", width=150):
+                        img = Image.open(uploaded_file)
+                        ocr_text = image_processing.run_ocr(img.copy())
+                        st.write(ocr_text)
+                        ui.process_result(f"Summarize the below text in {st.session_state.settings['language']}, explain like I am {st.session_state.settings['age']} years old in one paragraph. {ocr_text}")
+                        ui.display_result()
+                        ui.save_result()
+except Exception as e:
+    st.error("Please reload the page and try again. This may be due to an application issue or due to some of the content being rejected by the AI model.")
 
 ui.about()
