128db0d8435cf84c4e73b512dd1a1b784540c88e
arjuniyer01 <iyer9@wisc.edu>
2023-03-05 03:41:51 -0600

    voice updates

--- GOOGLE_APPLICATION_CREDENTIALS.py
+++
@@ -1,7 +0,0 @@
-import os
-
-# Set the path to your JSON key file
-json_key_path = "imposing-elixir-379617-9f22e9edde76"
-
-# Set the environment variable
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = json_key_path
--- avatar.py
+++ avatar.py
@@ -1,16 +1,19 @@
 import os
 from google.cloud import texttospeech
 import streamlit as st
+import json
+
+load_dotenv('.env')
 
 # set up credentials for Google Cloud TTS
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'imposing-elixir-379617-9f22e9edde76.json'
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = json(os.environ['GOOGLE_APPLICATION_CREDENTIALS'])
 
 def generate_voice(text: str):
     # create a client object for Google Cloud TTS
     client = texttospeech.TextToSpeechClient()
     # set up default voice configuration
     voice = texttospeech.VoiceSelectionParams(
-        language_code='hi-IN', ssml_gender=texttospeech.SsmlVoiceGender.FEMALE
+        language_code='en-US', ssml_gender=texttospeech.SsmlVoiceGender.FEMALE
     )
     # set up default audio configuration
     audio_config = texttospeech.AudioConfig(
