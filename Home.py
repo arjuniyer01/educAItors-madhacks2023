3c5a43116ffa2f12953aec76ed014f4cb7962067
arjuniyer01 <iyer9@wisc.edu>
2023-03-05 02:17:21 -0600

    whisper+gpt

--- Home.py
+++ Home.py
@@ -54,18 +54,17 @@ if user_input:
         ui.display_result()
         ui.save_result()
 elif uploaded_file:
-    if st.button("Audio"):
+    if uploaded_file.name.endswith(".mp3"):
         with st.spinner("Summarizing audio input..."):
             st.session_state.result_mode = True
             audio = pydub.AudioSegment.from_mp3(uploaded_file)
             audio.export("audio.mp3", format='mp3')
             audio_bytes = open("audio.mp3", 'rb').read()
             st.audio(audio_bytes, format=f'audio/.mp3', start_time=0)
-            whisper_text = ai.get_text_from_whisper()
-            st.write(whisper_text)
-            # ui.process_result(f"Summarize the below text in {settings['language']}, explain like I am {settings['age']} years old in one paragraph. {whisper_text}")
-            # ui.display_result()
-            # ui.save_result()
+            whisper_text = ai.get_text_from_whisper()["text"]
+            ui.process_result(f"Summarize the below text in {settings['language']}, explain like I am {settings['age']} years old in one paragraph. {whisper_text}")
+            ui.display_result()
+            ui.save_result()
 
     if st.button("Object detection"):
         with st.spinner("Summarizing using object detection..."):
