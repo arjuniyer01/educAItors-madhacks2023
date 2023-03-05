7649758d3fe407df2fdb21cb9cc21356c31e5309
arjuniyer01 <iyer9@wisc.edu>
2023-03-05 10:28:31 -0600

    logo test

--- pages/Settings.py
+++ pages/Settings.py
@@ -21,7 +21,6 @@ if not st.session_state.user_email:
     <a href={auth.get_login_str()}>
     <img src="https://lh3.googleusercontent.com/COxitqgJr1sJnIDe8-jiKhxDx1FrYbtRHKJ9z_hELisAlapwE9LUPh6fcXIfb5vwpbMl4xl9H9TRFPc5NOO8Sb3VSgIBrfRYvW6cUA" width="50" height="50">
     </a>
-    <br>
     ''', unsafe_allow_html=True)
     st.warning("Please login to access this page")
     st.stop()
