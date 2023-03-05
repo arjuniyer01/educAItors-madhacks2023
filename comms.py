0bb1fa86aeb801bb6882c6b2f8a79bb9bc523357
Arjun Iyer <56945234+arjuniyer01@users.noreply.github.com>
2023-03-05 04:37:21 -0600

    Update comms.py

--- comms.py
+++ comms.py
@@ -9,11 +9,11 @@ SENDGRID_API_KEY = st.secrets["SENDGRID_API_KEY"]
 
 # Using Twilio SendGrid's Python Library to send emails
 def send_email(email, message):
-    # sg = sendgrid.SendGridAPIClient(api_key=SENDGRID_API_KEY)
-    # from_email = Email("educaitors@gmail.com")
-    # to_email = To(email)
-    # subject = "Message from your Streamlit app"
-    # content = Content("text/plain", message)
-    # mail = Mail(from_email, to_email, subject, content)
-    # response = sg.client.mail.send.post(request_body=mail.get())
-    st.success(f"{message[0:10]}... sent to {email}")
+    sg = sendgrid.SendGridAPIClient(api_key=SENDGRID_API_KEY)
+    from_email = Email("educaitors@gmail.com")
+    to_email = To(email)
+    subject = "Message from your Streamlit app"
+    content = Content("text/plain", message)
+    mail = Mail(from_email, to_email, subject, content)
+    response = sg.client.mail.send.post(request_body=mail.get())
+    st.success(f"Email sent to {email}")
